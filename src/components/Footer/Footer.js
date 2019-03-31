f53e4c48b15269fb33d3f97e9b8170538b9ab07e
jlushington <jay.lushington@gmail.com>
2019-03-31 12:28:11 -0400

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './Footer.css';
 
 export default class Footer extends Component{
 
@@ -6,13 +7,26 @@ export default class Footer extends Component{
     render(){
 
        return(
-        <div className="navbar fixed-bottom">
-            <div className="container foot-bg-img">
-                footer
-            </div>
+        <div className="footer-bottom">
+        <div className="container">
+                            <div className="row">
+                                <div className="col-sm-6 ">
+                                    <div className="copyright-text">
+                                        <p>CopyRight © 2017 Digital All Rights Reserved</p>
+                                    </div>
+                                </div> 
+                                <div className="col-sm-6">							
+                                    <ul className="social-link pull-right">
+                                        <li><a href=""><span className="glyphicon glyphicon-heart-empty"></span></a></li>						
+                                        <li><a href=""><span className="glyphicon glyphicon-heart-empty"></span></a></li>
+                                        <li><a href=""><span className="glyphicon glyphicon-heart-empty"></span></a></li>
+                                        <li><a href=""><span className="glyphicon glyphicon-heart-empty"></span></a></li>
+                                        <li><a href=""><span className="glyphicon glyphicon-heart-empty"></span></a></li>
+                                    </ul>							
+                                </div> 
+                            </div>
+                        </div>
         </div>
-
-
        );
 
     }
