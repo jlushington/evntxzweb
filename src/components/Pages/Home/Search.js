e7563b8858762326b5aaae1e06486264bb05ba71
jlushington <jay.lushington@gmail.com>
2019-04-28 21:29:34 -0400

    Hide Search Nav

--- src/components/Pages/Home/Search.js
+++ src/components/Pages/Home/Search.js
@@ -6,14 +6,16 @@ export default class Search extends Component{
     render(){
         return(
             <div className="container">
-                <div className="row">
+                <div className="row p-t-50">
                     <div className="col-sm-12 searchnav font-75 color-grey">
+                    {/*
                             <ul>
                                 <li><a href="{#}">CONCERT</a></li>
                                 <li><a href="{#}">EVENTS</a></li>
                                 <li><a href="{#}">MEETUP</a></li>
                                 <li><a href="{#}">EATS</a></li>
                             </ul>
+                    */}
                         </div>
                 </div>
                 <div className="row pb-5 font-75 color-lightrey font-light">
